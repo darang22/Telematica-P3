5f0dac443e17b903cd5f48fabef323123a274144
Daniel Arango <skap.mks@gmail.com>
2017-04-29 14:11:05 -0500

    Se agrega la vista de resultados

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,11 @@
 class IndexController < ApplicationController
     def resultados
+        @parametros = params[:q]
+        @str = @parametros.gsub(/\s+/m, ' ').strip.split(" ")
+        @str = @str.join('\' OR word = \'')
+        sql = "select * from darang24 where word = '"+@str+"'"
+        @resultados = ActiveRecord::Base.connection.exec_query(sql)
+        @hash = @resultados.to_hash
+        
     end
 end
